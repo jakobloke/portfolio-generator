d2f5fea2cfb1c543b7aeb2a5b9cc080558b144d7
Jakob <61665831+jakobloke@users.noreply.github.com>
2020-05-06 23:52:39 -0700

    added inquirer

---
+++ app.js
@@ -0,0 +1,33 @@
+const inquirer = require('inquirer');
+
+inquirer
+  .prompt([
+    {
+      type: 'input',
+      name: 'name',
+      message: 'What is your name?'
+    }
+  ])
+  .then(answers => console.log(answers));
+
+// inquirer
+//   .prompt([
+//     {
+//       type: 'input',
+//       name: 'name',
+//       message: 'What is your name?'
+//     }
+//   ])
+//   .then(answers => console.log(answers));
+
+// const fs = require('fs');
+// const generatePage = require('./src/page-template.js');
+// const profileDataArgs = process.argv.slice(2, process.argv.length);
+// const [name, github] = profileDataArgs;
+
+// //first is file name, then data that is written, then the third is a call back function
+// fs.writeFile('./index.html', generatePage(name, github), err => {
+//     if (err) throw err;
+
+//     console.log('Portfolio complete! Check out index.html to see the output!')
+// });
--- src/page-template.js
+++ src/page-template.js
@@ -1,3 +1,4 @@
+  
 const generatePage = (name, github) => {
     return `
     <!DOCTYPE html> 
